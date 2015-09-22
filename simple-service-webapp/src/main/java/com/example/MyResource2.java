7b2c64df341abf61d91499199004da63d07f88ca
ahunigel <ahunigel@gmail.com>
2015-09-22 13:48:33 +0800

    add my own resource

---
+++ simple-service-webapp/src/main/java/com/example/MyResource2.java
@@ -0,0 +1,27 @@
+package com.example;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Root resource (exposed at "myresource" path)
+ */
+@Path("myresource2")
+public class MyResource2 {
+
+    /**
+     * Method handling HTTP GET requests. The returned object will be sent
+     * to the client as "text/plain" media type.
+     *
+     * @return String that will be returned as a text/plain response.
+     */
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getIt() {
+        return StringUtils.normalizeSpace("Got it!");
+    }
+}
